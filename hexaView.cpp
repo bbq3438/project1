77cc37117480d0d0a8422686c08d9c8db4c63198
bbq3438 <bbq3438@naver.com>
2016-09-21 12:14:37 +0900

    Add files via upload

--- hexaView.cpp
+++ hexaView.cpp
@@ -1,21 +1,12 @@
 #pragma once
 #include <stdio.h>
-//////////////////////////////////////////////////////////////////////////
-// Product Date : 2009/09/18                                            //
-// Author : (Last) Lee                                                  //
-//          (First) Hyunbin                                             //
-// comment : This function is printed hexa and ascii code               //
-//           입력되는 버퍼의 내용을 hexa(16진수)와                      //
-//           아스키 코드(ascii code)로 출력 시키는 함수                 //
-// 인지값 - INPUT : 포인터의 방향을 표시, 즉 입력이란 의미              //
-//		    const : 입력된 버퍼의 값은 함수 실행 도중에 변경되지 않음   //
-//          u_char *buffer : unsigned char pointer 형 변수 buffer       //
-// 반환값 - void                                                        //
-//////////////////////////////////////////////////////////////////////////
-void hexaView( const unsigned char *buffer, int size)
+void hexaView(const unsigned char *buffer, int size)
 {
 	int loop_temp = 0;
 	int counter = 0;
+
+	int line = size / 16;
+	int reminder = size % 16;
 
 	printf("=======================================================================\n");
 	printf("Address 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D");
@@ -24,37 +15,76 @@ void hexaView( const unsigned char *buffer, int size)
 
 	counter = 0;
 
-	if (1200 < size)
-		size = 1200;
-
-	while (counter < size)
+	while (counter < line)
 	{
 		printf("0x%04X	", counter);
 		loop_temp = 0;
-		while (loop_temp < 16)
+
+		if (counter != line - 1)
 		{
-			printf("%02X ", *buffer);
-			++buffer;
-			++loop_temp;
+			while (loop_temp < 16)
+			{
+				printf("%02X ", *buffer);
+				++buffer;
+				++loop_temp;
+			}
 		}
+		else
+		{
+			while (loop_temp < reminder)
+			{
+				printf("%02X ", *buffer);
+				++buffer;
+				++loop_temp;
+			}
+			for (loop_temp = 0; loop_temp < 16 - reminder; loop_temp++)
+				printf("   ");
+		}
+		
+
 		buffer = buffer - 16;
 		loop_temp = 0;
-		while (loop_temp < 16)
-		{
-			if (31 > *buffer || 127 <= *buffer)
-			{
-				putchar('.');
-			}
-			else
-			{
-				putchar(*buffer);
 
+		if (counter != line - 1)
+		{
+			while (loop_temp < 16)
+			{
+				if (31 > *buffer || 127 <= *buffer)
+				{
+					putchar('.');
+				}
+				else
+				{
+					putchar(*buffer);
+
+				}
+
+				++buffer;
+				++loop_temp;
 			}
-			++buffer;
-			++loop_temp;
 		}
+		else
+		{
+			while (loop_temp < reminder)
+			{
+				if (31 > *buffer || 127 <= *buffer)
+				{
+					putchar('.');
+				}
+				else
+				{
+					putchar(*buffer);
+
+				}
+
+				++buffer;
+				++loop_temp;
+			}
+		}
+						
 		counter = counter++;
 		putchar('\n');
 	}
+
 	return;
 }
